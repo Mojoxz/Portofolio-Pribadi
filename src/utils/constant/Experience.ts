f8d25d2ff95bdb0e171d1c404876aa775a6f3fe9
Firmandhani <dhanimojo01@gmail.com>
2026-01-25 23:01:25 +0700

    Perubahan pada page about'

--- src/utils/constant/Experience.ts
+++ src/utils/constant/Experience.ts
@@ -9,32 +9,42 @@ interface IExperience {
   date: string
 }
 export const experience = [
+
+  {
+    title: 'Coding Lecturer Junior High School',
+    company: 'SD Negri KedungMungal',
+    type: 'Work',
+    description:
+      'I teach at Kedung Mungal Elementary School as an extracurricular coding teacher. I use Scratch to develop logic in children.',
+    date: 'August 2025 - Present',
+    url: 'https://maps.app.goo.gl/MwvftcyWuHDVMMFe7',
+  },
   {
     title: 'Full Stack Developer Intern',
     company: 'Dinas Perhubungan Kota Surabaya',
-    type: 'Internship',
+    type: 'Work',
     description:
       'At the Dinas Perhubungan Kota Surabaya, I created a website to manage Surabaya\'s bus stops using Laravel. Admins can use CRUD and display bus stop locations based on coordinates.',
     date: 'August 2025 - December 2025',
     url: 'https://halte.dishubsurabaya.info/',
   },
   {
-    title: 'Mobile Developer Cohort',
-    company: 'Bangkit Academy',
-    type: 'Education',
-    description:
-      'The Mobile Developer Cohort at Bangkit Academy is an intensive program from February to July 2024, focused on Android development and integrating machine learning into Android apps. Participants will work on hands-on projects and receive expert mentorship, culminating in a robust portfolio of Android applications.',
-    date: 'Febuary - July 2024',
-    url: 'https://grow.google/intl/id_id/bangkit?tab=machine-learning',
-  },
-  {
-    title: 'Lab Assistant',
-    company: 'STMIK Widya Cipta Dharma',
+    title: 'Network Engineer',
+    company: 'SMK Negeri 1 Mojoanyar',
     type: 'Work',
     description:
-      'support the operation and success of practicum activities and setting up the necessary equipment and softwareensuring laboratory safety and order and providing assistance to students during practical sessions.',
-    date: 'April 2023 - April 2024',
-    url: 'https://www.wicida.ac.id/',
+      'Conducting network maintenance, installing fiber optic networks at 19 installation points, and regular network maintenance.',
+    date: 'January - October 2023',
+    url: 'https://www.instagram.com/skanemo_official/',
+  },
+  {
+    title: 'IT Support',
+    company: 'SMK Negeri 1 Pungging',
+    type: 'Work',
+    description:
+      'make repairs, design networks, install radio networks, troubleshoot teacher laptops, repair networks on servers and server side.',
+    date: 'January 2021 - April 2023',
+    url: 'https://www.smkn1pungging.sch.id/',
   },
   
 ]
